7eec532280be5f023a03951b03da85cfef95a98b
rogec540 <rogec540@newschool.edu>
2024-02-26 15:11:22 -0500

    adding boundaries

--- sketch.js
+++ sketch.js
@@ -17,99 +17,120 @@ let move;
 let r, g, b;
 
 function preload() {
-	partyConnect('wss://demoserver.p5party.org', 'team1_gameA');
-	guests = partyLoadGuestShareds();
-	my = partyLoadMyShared();
-	shared = partyLoadShared('shared', {});
+  partyConnect("wss://demoserver.p5party.org", "team1_gameA");
+  guests = partyLoadGuestShareds();
+  my = partyLoadMyShared();
+  shared = partyLoadShared("shared", {});
 
-	// DESIGN VALUE: Goated graphics
-	bg = loadImage('./assets/GameAMap2.png');
+  // DESIGN VALUE: Goated graphics
+  bg = loadImage("./assets/GameAMap2.png");
 }
 
 function setup() {
-	createCanvas(640, 640);
+  createCanvas(640, 640);
 
-	my.x = 294;
-	my.y = 610;
-	move = 5;
+  my.x = 294;
+  my.y = 610;
+  move = 3;
 
-	r = random(255);
-	g = random(255);
-	b = random(255);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
-	drawGame();
-	drawPlayers();
-	checkPressedKeys();
-	checkBoundaries();
+  drawGame();
+  drawPlayers();
+  checkPressedKeys();
+  checkBoundaries();
 }
 
 function drawGame() {
-	background(bg);
+  background(bg);
 }
 
 function drawPlayers() {
-	//draw entryway
-	push();
-	noStroke();
-	fill(63, 24, 1);
-	rect(289, 632, 30, 7, 4);
-	pop();
+  //draw entryway
+  push();
+  noStroke();
+  fill(63, 24, 1);
+  rect(289, 632, 30, 7, 4);
+  pop();
 
-	//initialize players
-	for (const guest of guests) {
-		fill(r, g, b);
-		rect(guest.x, guest.y, 20, 20);
-	}
+  //initialize players
+  for (const guest of guests) {
+    fill(r, g, b);
+    rect(guest.x, guest.y, 20, 20);
+  }
 }
 
 // DESIGN VALUE: Easy access
 // uses common keys to move
 function checkPressedKeys() {
-	if (keyIsDown(LEFT_ARROW) || keyIsDown(65 /*a*/)) {
-		my.x -= move;
-	} else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68 /*d*/)) {
-		my.x += move;
-	} else if (keyIsDown(UP_ARROW) || keyIsDown(87 /*w*/)) {
-		my.y -= move;
-	} else if (keyIsDown(DOWN_ARROW) || keyIsDown(83 /*s*/)) {
-		my.y += move;
-	} else my.keysReleasedSinceAction = true;
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65 /*a*/)) {
+    my.x -= move;
+  } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68 /*d*/)) {
+    my.x += move;
+  } else if (keyIsDown(UP_ARROW) || keyIsDown(87 /*w*/)) {
+    my.y -= move;
+  } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83 /*s*/)) {
+    my.y += move;
+  } else my.keysReleasedSinceAction = true;
+}
+
+function keyPressed() {
+  //reset position with 'Ctrl' key
+  if (keyCode === CONTROL) {
+    my.x = 294;
+    my.y = 610;
+    redraw();
+  }
 }
 
 function checkBoundaries() {
-	if (my.x < 0) {
-		//left and top wall
-		my.x += move;
-	} else if (my.x > 619) {
-		//right wall
-		my.x -= move;
-	} else if (my.y < 0) {
-		//top wall
-		my.y += move;
-	} else if (my.y > 619) {
-		//bottom wall
-		my.y -= move;
-	} else if (my.x > 230 && my.x < 350 && my.y < 90) {
-		//piano
-		move *= -1;
-	} else if (my.x < 98 && my.y > 110 && my.y < 190) {
-		//left curtain
-		move *= -1;
-	} else if (my.x < 30 && my.y > 212 && my.y < 295) {
-		//left countertop
-		move *= -1;
-	} else if (my.x > 588 && my.y > 212 && my.y < 295) {
-		//right countertop
-		move *= -1;
-	} else if (my.x > 80 && my.x < 125 && my.y > 586) {
-		//bottom roundtable
-		move *= -1;
-	} else if (my.x > 555 && my.x < 605 && my.y > 550 && my.y < 605) {
-		//chess table
-		move *= -1;
-	} else {
-		move = 5;
-	}
+  if (my.x < 0) {
+    //left wall
+    my.x += move;
+  } else if (my.x > 619) {
+    //right wall
+    my.x -= move;
+  } else if (my.y < 0) {
+    //top wall
+    my.y += move;
+  } else if (my.y > 619) {
+    //bottom wall
+    my.y -= move;
+  } else if (my.x > 230 && my.x < 350 && my.y < 90) {
+    //piano
+    move *= -1;
+  } else if (my.x < 98 && my.y > 110 && my.y < 195) {
+    //left curtain
+    move *= -1;
+  } else if (my.x > 490 && my.y > 110 && my.y < 195) {
+    // right curtain
+    move *= -1;
+  } else if (my.x > 110 && my.x < 290 && my.y > 140 && my.y < 181) {
+    //left stagefront
+    move *= -1;
+  } else if (my.x > 300 && my.x < 478 && my.y > 140 && my.y < 181) {
+    //right stagefront
+    move *= -1;
+  } else if (my.x > 43 && my.x < 94 && my.y < 67) {
+    //top left sidetable
+    move *= -1;
+  } else if (my.x < 30 && my.y > 212 && my.y < 295) {
+    //left countertop
+    move *= -1;
+  } else if (my.x > 588 && my.y > 212 && my.y < 295) {
+    //right countertop
+    move *= -1;
+  } else if (my.x > 80 && my.x < 125 && my.y > 586) {
+    //bottom roundtable
+    move *= -1;
+  } else if (my.x > 555 && my.x < 605 && my.y > 550 && my.y < 605) {
+    //chess table
+    move *= -1;
+  } else {
+    move = 3;
+  }
 }
